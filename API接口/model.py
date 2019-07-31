3a0048778b89242b25e9a34fcc05e6d0724ad612
BeyondLam <44283585+BeyondLam@users.noreply.github.com>
2019-08-01 00:00:09 +0800

    Create model.py
    数据库的python代码

---
+++ API接口/model.py
@@ -0,0 +1,122 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+# 会员
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    name = db.Column(db.String(100), unique=True)  # 昵称
+    pwd = db.Column(db.String(100))  # 密码
+    sex = db.Column(db.Integer)  # 性别
+    email = db.Column(db.String(100), unique=True)  # 邮箱
+    phone = db.Column(db.String(11), unique=True)  # 手机号码
+    school = db.Column(db.String(100))  # 学校
+    level = db.Column(db.String(100))  # 年级
+    face = db.Column(db.String(255), unique=True)  # 头像
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)  # 注册时间
+    comments = db.relationship('Comment', backref='user')  # 评论外键关系关联
+    blogcol = db.relationship('Blogcol', backref='user')  # 博客收藏外键关系关联
+    search = db.relationship('Search', backref='user')  # 搜索历史外键关系关联
+    chat = db.relationship('Chat', backref='user')  # 搜索历史外键关系关联
+    def __repr__(self):
+        return "<User %r>" % self.name
+
+
+# 博客
+class Blog(db.Model):
+    __tablename__ = "blog"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    title = db.Column(db.String(255), unique=True)  # 标题
+    content = db.Column(db.Text)  # 内容
+    author = db.Column(db.String(100))  # 作者
+    create_time = db.Column(db.DateTime, index=True, default=datetime.now)  # 添加时间
+    num_of_view = db.Column(db.Integer, default=0)  # 浏览次数
+    face = db.Column(db.String(255))  # 头像的地址
+    blog_url = db.Column(db.String(255))  # 博客的地址
+    commentnum = db.Column(db.BigInteger)  # 评论量
+    comments = db.relationship('Comment', backref='blog')  # 评论外键关系关联
+    blogcol = db.relationship('Blogcol', backref='blog')  # 博客收藏外键关系关联
+
+    def __repr__(self):
+        return "<Blog %r>" % self.name
+
+
+# 评论
+class Comment(db.Model):
+    __tablename__ = "comment"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    content = db.Column(db.Text)  # 内容
+    blog_id = db.Column(db.Integer, db.ForeignKey('blog.id'))  # 所属博客
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
+    competition_id = db.Column(db.Integer, db.ForeignKey('competition.id'))  # 所属竞赛
+    activity_id = db.Column(db.Integer, db.ForeignKey('activity.id'))  # 所属活动
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)  # 添加时间
+
+    def __repr__(self):
+        return "<Comment %r>" % self.id
+
+
+# 博客收藏
+class Blogcol(db.Model):
+    __tablename__ = "blogcol"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    blog_id = db.Column(db.Integer, db.ForeignKey('blog.id'))  # 所属博客
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
+    add_time = db.Column(db.DateTime, index=True, default=datetime.now)  # 添加时间
+
+    def __repr__(self):
+        return "<Blogcol %r>" % self.id
+
+
+# 竞赛
+class Competition(db.Model):
+    __tablename__ = "competition"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    title = db.Column(db.String(255), unique=True)  # 标题
+    content = db.Column(db.Text)  # 内容
+    author = db.Column(db.String(100))  # 作者
+    num_of_view = db.Column(db.Integer, default=0)  # 浏览次数
+    create_time = db.Column(db.DateTime, index=True, default=datetime.now)  # 添加时间
+    comments = db.relationship('Comment', backref='competition')  # 评论外键关系关联
+    search = db.relationship('Search', backref='competition')  # 搜索历史外键关系关联
+
+
+# 活动
+class Activity(db.Model):
+    __tablename__ = "activity"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    title = db.Column(db.String(255), unique=True)  # 标题
+    content = db.Column(db.Text)  # 内容
+    author = db.Column(db.String(100))  # 作者
+    num_of_view = db.Column(db.Integer, default=0)  # 浏览次数
+    create_time = db.Column(db.DateTime, index=True, default=datetime.now)  # 添加时间
+    comments = db.relationship('Comment', backref='activity')  # 评论外键关系关联
+    search = db.relationship('Search', backref='activity')  # 搜索历史外键关系关联
+
+
+# 搜索历史
+class Search(db.Model):
+    __tablename__ = "search"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    keyword = db.Column(db.String(255))  # 搜索关键字
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
+    competition_id = db.Column(db.Integer, db.ForeignKey('competition.id'))  # 所属竞赛
+    activity_id = db.Column(db.Integer, db.ForeignKey('activity.id'))  # 所属活动
+
+
+# 关注
+class Like(db.Model):
+    __tablename__ = "like"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    blog_id = db.Column(db.Integer, db.ForeignKey('blog.id'))  # 所属博客
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
+    # user_to_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
+
+class Chat(db.Model):
+    __tablename__ = "chat"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
+    user_to_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
