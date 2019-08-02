e13e3fc26dd3f10254a56a1d5388f6ec479e95eb
BeyondLam <382552192@qq.com>
2019-08-02 16:25:05 +0800

    数据库修改了一些错误

--- API接口/model.py
+++ API接口/model.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-# from app import db
+
+# from . import db
 
 """
 from flask import Flask
@@ -11,6 +12,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'xxx'
 db = SQLAlchemy(app)
 """
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
 
 
 # 会员
@@ -29,7 +39,7 @@ class User(db.Model):
     comments = db.relationship('Comment', backref='user')  # 评论外键关系关联
     blogcol = db.relationship('Blogcol', backref='user')  # 博客收藏外键关系关联
     search = db.relationship('Search', backref='user')  # 搜索历史外键关系关联
-    chat = db.relationship('Chat', backref='user')  # 搜索历史外键关系关联
+    chat = db.relationship('Chat', backref='user')  # 聊天私聊外键关系关联
 
     def __repr__(self):
         return "<User %r>" % self.name
@@ -41,7 +51,7 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # 编号
     title = db.Column(db.String(255), unique=True)  # 标题
     content = db.Column(db.Text)  # 内容
-    author = db.Column(db.String(100))  # 作者
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
     create_time = db.Column(db.DateTime, index=True, default=datetime.now)  # 添加时间
     num_of_view = db.Column(db.Integer, default=0)  # 浏览次数
     face = db.Column(db.String(255))  # 头像的地址
@@ -52,6 +62,7 @@ class Blog(db.Model):
 
     def __repr__(self):
         return "<Blog %r>" % self.name
+
 
 
 # 评论
@@ -67,6 +78,8 @@ class Comment(db.Model):
 
     def __repr__(self):
         return "<Comment %r>" % self.id
+
+
 
 
 # 博客收藏
@@ -113,8 +126,9 @@ class Search(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # 编号
     keyword = db.Column(db.String(255))  # 搜索关键字
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
-    competition_id = db.Column(db.Integer, db.ForeignKey('competition.id'))  # 所属竞赛
-    activity_id = db.Column(db.Integer, db.ForeignKey('activity.id'))  # 所属活动
+    cp_or_act = db.Column(db.Integer) # 数字１代表活动，数字２代表竞赛
+    #competition_id = db.Column(db.Integer, db.ForeignKey('competition.id'))  # 所属竞赛
+    #activity_id = db.Column(db.Integer, db.ForeignKey('activity.id'))  # 所属活动
 
 
 # 关注
@@ -130,9 +144,58 @@ class Like(db.Model):
 class Chat(db.Model):
     __tablename__ = "chat"
     id = db.Column(db.Integer, primary_key=True)  # 编号
+    content = db.Column(db.String(255))  # 聊天内容
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
-    user_to_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
+    # user_to_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
 
 
 if __name__ == "__main__":
-# db.create_all()
+
+
+
+"""
+    cp = Competition.query.get(2)
+    print(cp)
+    cps = cp.comments
+    for cpps in cps:
+        print(cpps.id)
+
+
+
+    u = User.query.get(2)
+    print(u)
+    coms = u.comments
+    for com in coms:
+        print(com.content)
+
+    comment1 = Comment(content="评论内容测试１", user_id=1, competition_id=1)
+    comment2 = Comment(content="评论内容测试2", user_id=2, competition_id=1)
+    comment3 = Comment(content="评论内容测试3", user_id=2, competition_id=2)
+
+    db.session.add(comment1)
+    db.session.add(comment2)
+    db.session.add(comment3)
+
+    db.session.commit()
+    print("yesss")
+    cop1 = Competition(title="标题测试１", content="内容测试１", author="网站发布人1")
+    cop2 = Competition(title="标题测试２", content="内容测试2", author="网站发布人2")
+
+    db.session.add(cop1)
+    db.session.add(cop2)
+
+
+    db.session.commit()
+    print("yes!")
+    db.create_all()
+    user1 = User(name="lmp", pwd='123456', sex=1, email='382552192@qq.com', phone=123456789, school="hnnydx",
+                 level="大二", face="/root/face/123456.png")
+    user2 = User(name="lzk", pwd='12345622', sex=2, email='38255219222@qq.com', phone=1234522222, school="hnnydx222",
+                 level="大22", face="/root/face/123456222.png")
+
+    db.session.add(user1)
+    db.session.add(user2)
+
+    db.session.commit()
+    print("hello")
+"""
